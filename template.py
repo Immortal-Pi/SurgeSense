ca02ebdfde97400b2b064845f5d3770ef7d7ba86
amruth pai <26.amruth@gmail.com>
2025-03-11 16:17:39 -0500

    'project

--- template.py
+++ template.py
@@ -11,17 +11,23 @@ list_of_files=[
     f'src/{project_name}/__init__.py',
     f'src/{project_name}/components/__init__.py',
     f'src/{project_name}/utils/__init__.py',
+    f'src/{project_name}/utils/common.py',
     f'src/{project_name}/config/__init__.py',
+    f'src/{project_name}/config/configuration.py',
     f'src/{project_name}/pipeline/__init__.py',
     f'src/{project_name}/entity/__init__.py',
+     f'src/{project_name}/entity/config_entity.py',
     f'src/{project_name}/constants/__init__.py',
     'config/config.yaml',
+    'params.yaml',
+    'schema.yaml',
+    'main.py',
+    'Dockerfile',
     'dvc.yaml',
     'requirements.txt',
     'setup.py',
-    'research/trails.ipynb', 
-    'template/index.html', # for the flask app 
-    'test.py',
+    'research/research.ipynb', 
+    'template/index.html' # for the flask app 
 
 ]
 
